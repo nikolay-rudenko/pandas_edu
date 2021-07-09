7faf5c566f713359e4eb1830296d98b11ed04196
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-07-09 15:03:59 +0300

    Additional improving

---
+++ pandas_course/exercise/ex_1.py
@@ -0,0 +1,40 @@
+###############################################################
+# DO NOT DELETE THIS CODE. IT IS NEEDED FOR THE TESTS TO RUN. #
+from pprint import pprint
+# from unittest.mock import MagicMock                           #
+import pandas as pd                                           #
+# pd.Series = MagicMock()                                       #
+###############################################################
+
+# Assume the pandas library has already been imported and assigned
+# the alias "pd".
+
+# Create a list with 4 countries - United States, France, Germany, Italy
+countries = ['United States', 'France', 'Germany', 'Italy']
+
+# Create a new Series by passing in the list of countries
+pd.Series(countries)
+# Assign the Series to a "countries" variable
+countries = pd.Series(countries)
+
+# Create a list with 3 colors - red, green, blue
+colors = ['red', 'green', 'blue']
+# Create a new Series by passing in the list of colors
+pd.Series(colors)
+
+# Assign the Series to a "colors" variable
+colors = pd.Series(colors)
+
+# Given the "recipe" dictionary below,
+# create a new Series by passing in the dictionary as the data source
+# Assign the resulting Series to a "series_dict" variable
+
+recipe = {
+  "Flour": True,
+  "Sugar": True,
+  "Salt": False
+}
+
+series_dict = pd.Series(recipe)
+
+print(series_dict)
