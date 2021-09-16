0bc0181ac73ce73d6ccd82e5cbc26b978a3f5b4f
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-09-16 21:22:23 +0300

    Additional improving

---
+++ pandas_course/exercise/ex_5.py
@@ -0,0 +1,23 @@
+###############################################################
+# DO NOT DELETE THIS CODE. IT IS NEEDED FOR THE TESTS TO RUN. #
+import pandas as pd                                             #
+###############################################################
+
+# Assume the pandas library has already been imported and assigned an alias of 'pd'.
+# Let's say I have a DataFrame that looks like this:
+#
+#     A	  B	  C	  D	  E
+#  0  7	  8	  9	  7	  5
+#  1  7	  5	  3	  6	  9
+#  2  3	  8	  8	  4	  4
+#
+# The DataFrame has been assigned to the variable "df"
+
+
+# Write the code to extract the "C" column from the DataFrame.
+
+
+# Write the code to extract the "E" column from the DataFrame.
+
+
+# Write the code to extract the "A" column from the DataFrame.
